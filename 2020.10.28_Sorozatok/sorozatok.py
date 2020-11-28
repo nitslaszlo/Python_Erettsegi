ca3058d3625792dc009fa882c051152a98d06202
László Nits <nitslaszlo@gmail.com>
2020-11-28 20:10:12 +0100

    up

--- 2020.10.28_Sorozatok/sorozatok.py
+++ 2020.10.28_Sorozatok/sorozatok.py
@@ -6,10 +6,10 @@ def main():
     print(f'2. feladat\nA listában {m.ismert_a_vetites_datuma_darab} db vetítési dátummal rendelkező epizód van.')
     print(f'3. feladat\nA listában lévő epizódok {m.megnezve_arany:.2%}-át látta.')
     print(f'4. feladat\nSorozatnézéssel {m.ido_nap} napot {m.ido_ora} órát és {m.ido_perc} percet töltött.')
-    for i in m.nem_latta_meg(input('5. feladat\nAdjon meg egy dátumot! Dátum= ')):
-        print(i)
-    for i in m.adott_napon_vetitett(input('7. feladat\nAAdja meg a hét egy napját (például cs)! Nap= ')):
-        print(i)
+    for e in m.nem_latta_meg(input('5. feladat\nAdjon meg egy dátumot! Dátum= ')):
+        print(e)
+    for e in m.adott_napon_vetitett(input('7. feladat\nAAdja meg a hét egy napját (például cs)! Nap= ')):
+        print(e)
     # 8. feladat:
     m.write_stat('summa.txt')
 
