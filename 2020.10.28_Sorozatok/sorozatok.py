9fcc6c0433ce2f8b6a2592116a5db839d40d1873
László Nits <nitslaszlo@gmail.com>
2020-11-28 20:10:35 +0100

    up

--- 2020.10.28_Sorozatok/sorozatok.py
+++ 2020.10.28_Sorozatok/sorozatok.py
@@ -8,7 +8,7 @@ def main():
     print(f'4. feladat\nSorozatnézéssel {m.ido_nap} napot {m.ido_ora} órát és {m.ido_perc} percet töltött.')
     for e in m.nem_latta_meg(input('5. feladat\nAdjon meg egy dátumot! Dátum= ')):
         print(e)
-    for e in m.adott_napon_vetitett(input('7. feladat\nAAdja meg a hét egy napját (például cs)! Nap= ')):
+    for e in m.adott_napon_vetitett(input('7. feladat\nAdja meg a hét egy napját (például cs)! Nap= ')):
         print(e)
     # 8. feladat:
     m.write_stat('summa.txt')
