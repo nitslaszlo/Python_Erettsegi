1651a9f6b473de8d7a1e90a1c7138147f2c55469
László Nits <nitslaszlo@gmail.com>
2020-11-28 20:01:28 +0100

    up

--- 2020.05.18i_Menetrend/solution.py
+++ 2020.05.18i_Menetrend/solution.py
@@ -12,15 +12,15 @@ class Solution(object):
 
     def __init__(self, source_file: str) -> None:
         with open(source_file, 'r', encoding='UTF8') as sr:
-            for i in sr.read().splitlines():
-                current_train = Train(i)
+            for line in sr.read().splitlines():
+                current_train = Train(line)
                 if current_train.time_is_depart and not current_train.is_first_station:
                     previous_data: List[Train] = list(filter(lambda x: x.id == current_train.id, self._trains_data))
                     current_train.calculate_downtime(previous_data[-1].time)
                 self._trains_data.append(current_train)
-        for i in self._trains_data:
-            self._trains.add(i.id)
-            self._stations.add(i.station)
+        for e in self._trains_data:
+            self._trains.add(e.id)
+            self._stations.add(e.station)
 
     def train_running_time_check(self, train_id: int) -> str:
         train_data: List[Train] = list(filter(lambda x: x.id == train_id, self._trains_data))
@@ -35,24 +35,24 @@ class Solution(object):
     def write_data(self, train_id: int) -> None:
         train_data: List[Train] = list(filter(lambda x: x.id == train_id, self._trains_data))
         with open(f'halad{train_id}.txt', 'w', encoding='UTF8') as sw:
-            for i in train_data:
-                if not i.time_is_depart:
-                    sw.write(f'{i.station}. állomás: {i.time.hour}:{i.time.minute}\n')
+            for e in train_data:
+                if not e.time_is_depart:
+                    sw.write(f'{e.station}. állomás: {e.time.hour}:{e.time.minute}\n')
 
     def where_are_the_trains(self, time_string: str) -> str:
         output: str = ''
         input_time = datetime.datetime(2020, 1, 1, int(time_string.split(' ')[0]), int(time_string.split(' ')[1]))
-        for i in self._trains:
-            t: List[Train] = list(filter(lambda x: x.id == i, self._trains_data))
-            for c in range(1, len(t)):
-                p = c - 1  # index of previous event
+        for e in self._trains:
+            t: List[Train] = list(filter(lambda x: x.id == e, self._trains_data))
+            for i in range(1, len(t)):
+                p = i - 1  # index of previous event
                 if t[p].time_is_depart:
-                    if t[p].time <= input_time < t[c].time:
+                    if t[p].time <= input_time < t[i].time:
                         output += \
-                            f'A(z) {t[c].id}. vonat a {t[p].station}. és a {t[c].station}. állomás között járt.\n'
+                            f'A(z) {t[i].id}. vonat a {t[p].station}. és a {t[i].station}. állomás között járt.\n'
                 else:  # is_arrival
-                    if t[p].time <= input_time < t[c].time:
-                        output += f'A(z) {t[c].id}. vonat a {t[c].station}. állomáson állt.\n'
+                    if t[p].time <= input_time < t[i].time:
+                        output += f'A(z) {t[i].id}. vonat a {t[i].station}. állomáson állt.\n'
         return output
 
     @property
