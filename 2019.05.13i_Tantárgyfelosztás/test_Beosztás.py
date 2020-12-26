4a824758f8b1877479db7fbc491677fdf53a0196
László Nits <nitslaszlo@gmail.com>
2020-12-26 09:16:12 +0100

    up

---
+++ 2019.05.13i_Tantárgyfelosztás/test_Beosztás.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from Beosztás import Beosztás
+
+
+class TestFelszállásJegy(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.beosztás1: Beosztás = Beosztás(['Albatrosz Aladin', 'biologia', '9.a', '2'])
+
+    def test_név(self):
+        self.assertEqual(self.beosztás1.név, 'Albatrosz Aladin')
+
+    def test_órszám(self):
+        self.assertEqual(self.beosztás1.óraszám, 2)
+
+    def test_tantárgy(self):
+        self.assertEqual(self.beosztás1.tantárgy, 'biologia')
+
+    def test_osztály(self):
+        self.assertEqual(self.beosztás1.osztály, '9.a')
---
+++ 2019.05.13i_Tantárgyfelosztás/test_Megoldás.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+from Megoldás import Megoldás
+import filecmp
+
+
+class TestMegoldás(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.megoldás1: Megoldás = Megoldás('beosztas.txt')
+
+    def test_bejegyzések_száma(self):
+        self.assertEqual(self.megoldás1.bejegyzések_száma, 329)
+
+    def test_heti_összóraszám(self):
+        self.assertEqual(self.megoldás1.összóraszám, 1016)
+
+    def test_tanári_óraszám(self):
+        self.assertEqual(self.megoldás1.tanári_óraszám('Albatrosz Aladin'), 24)
+
+    def test_ofőket_ír(self):
+        self.megoldás1.ofőket_ír('of.txt')
+        self.assertTrue(filecmp.cmp('of.txt', 'of_OH.txt', shallow=False))
+
+    def test_tanári_csoportbontás_van(self):
+        self.assertEqual(self.megoldás1.csoportbontás_van('10.b', 'kemia'), True)
+
+    def test_tanárok_száma(self):
+        self.assertEqual(self.megoldás1.tanárok_száma, 49)
